6613997fc74e3c5e54e88c9809e373dae2dc554b
Leonardo Hernandes <leonardo.hernandes@setaatacadista.com.br>
2017-01-20 04:29:51 -0200

    Commit inicial. Cópia do vraptor-blank-project

---
+++ src/main/java/br/com/leohernandes/agendador/controller/IndexController.java
@@ -0,0 +1,23 @@
+package br.com.leohernandes.agendador.controller;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Result;
+
+@Controller
+public class IndexController {
+
+	private final Result result;
+	
+	@Inject
+	public IndexController(Result result) {
+		this.result = result;
+	}
+
+	@Path("/")
+	public void index() {
+		result.include("variable", "VRaptor!");
+	}
+}
